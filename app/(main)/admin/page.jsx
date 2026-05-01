beae726c8374ed937e79c6cb96c96d3ccc5b51ed
Blue-Onion <rawatadityasingh963@gmail.com>
2026-05-01 20:04:36 +0530

    feat: implement admin dashboard tabs for event creation and account/art approval workflows

--- app/(main)/admin/page.jsx
+++ app/(main)/admin/page.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 export default function Page() {
     return (
         <section id='adminPage'>
-g
+            <Tabs defaultValue="create-Event" className="w-full h-full bg-red-600 grid-cols-2">
+                <div className="">
+
+                <TabsList>
+                    <TabsTrigger value="create-Event">Create-Event</TabsTrigger>
+                    <TabsTrigger value="approve">Approve</TabsTrigger>
+                </TabsList>
+                </div>
+                <div className="">
+
+                <TabsContent value="create-Event">Make changes to your create-Event here.</TabsContent>
+                <TabsContent value="approve">Change your approve here.</TabsContent>
+                </div>
+            </Tabs>
         </section>
     )
 }

151547c6c948d219dab0571b555927064df393ca
Blue-Onion <rawatadityasingh963@gmail.com>
2026-05-06 06:36:01 +0530

    My red accent

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
+    className="dark"
       lang="en"
       data-scroll-behavior="smooth"
       >
